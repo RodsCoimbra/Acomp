e764577d172025e56e8759c1507b9ce450bbef9e
Rodrigo Coimbra <rodscoimbra@gmail.com>
2021-04-11 01:00:25 +0100

    Delete PB2 directory

--- PB2/1(a,b).s
+++
@@ -1,6 +0,0 @@
-addi x10, x0, 0x003
-addi x11, x0, 0x007
-li x12, 0x004
-add x11, x11, x10
-sub x11, x11, x12
-sll x10, x11, x10
--- PB2/1(d).s
+++
@@ -1,4 +0,0 @@
-li x2, 824
-li x3, 0b100000000
-and x3, x2, x3
-xor x2, x3, x2 
